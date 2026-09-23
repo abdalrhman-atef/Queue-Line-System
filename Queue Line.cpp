905285c4218d0e9c98db5247428404052528f045
Abdelrhman Atef <abdalrhman.programming@gmail.com>
2026-09-23 23:06:37 +0300

    Add queue line system project

---
+++ Queue Line.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include"clsQueueLine.h"
+using namespace std;
+int main()
+{
+	clsQueueLine PayBillsQueue("A0",10);
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+
+	cout << "Pay Bills Queue Info :\n";
+	PayBillsQueue.PrintInfo();
+	PayBillsQueue.PrintTicketsLineLTR();
+	PayBillsQueue.ServeNextClient();
+	cout<<"Served Clients :"<< PayBillsQueue.ServedClients()<<"\n";
+	cout << "Next CLient is :" << PayBillsQueue.WhoIsNext()<<endl;
+
+}
